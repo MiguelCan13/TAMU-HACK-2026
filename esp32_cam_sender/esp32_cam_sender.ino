cbadc4da58d377ca2994a77bca79703a711e8d74
Sam <35083571+Samthecoolman@users.noreply.github.com>
2026-01-19 02:17:55 -0600

    Update esp32_cam_sender.ino
    pooper

--- esp32_cam_sender/esp32_cam_sender.ino
+++ esp32_cam_sender/esp32_cam_sender.ino
@@ -21,6 +21,27 @@
 #define HREF_GPIO_NUM 23
 #define PCLK_GPIO_NUM 22
 
+#define TRIG_PIN 4  
+#define ECHO_PIN 16 
+#define THRESHOLD_CM 30
+
+long getDistance() {
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+  long duration = pulseIn(ECHO_PIN, HIGH, 30000); 
+  long cm = (duration * 0.034 / 2);
+  
+  // Set pins to neutral to avoid Camera Bus interference
+  pinMode(TRIG_PIN, INPUT);
+  pinMode(ECHO_PIN, INPUT);
+  return cm;
+}
+
 RF24 radio(2, 15); 
 const byte address[] = "00001";
 
@@ -31,6 +52,10 @@ struct IndexedChunk {
 
 void setup() {
   Serial.begin(115200);
+
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  digitalWrite(TRIG_PIN, LOW);
   
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
@@ -45,8 +70,9 @@ void setup() {
   config.pin_pwdn = PWDN_GPIO_NUM; config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
   config.pixel_format = PIXFORMAT_RGB565;
-  config.frame_size = FRAMESIZE_QVGA; 
+  config.frame_size = FRAMESIZE_QQVGA; 
   config.fb_count = 1;
+  config.fb_location = CAMERA_FB_IN_DRAM;
 
   esp_camera_init(&config);
   sensor_t * s = esp_camera_sensor_get();
@@ -72,23 +98,31 @@ void setup() {
 }
 
 void loop() {
-  camera_fb_t * fb = esp_camera_fb_get();
-  if (!fb) return;
+  long distance = getDistance();
+  
+  if (distance > 0 && distance < THRESHOLD_CM) {
 
-  uint16_t* raw_pixels = (uint16_t*)fb->buf;
-  IndexedChunk chunk;
+    pinMode(16, INPUT); // Release the pin immediately
+    delay(200);         // Wait for electrical noise to settle
+    camera_fb_t * fb = esp_camera_fb_get();
+    if (!fb) return;
 
-  Serial.println("Sending frame...");
-  for (uint32_t i = 0; i < 76800; i += 14) {
-    chunk.pixel_index = i;
-    for (int j = 0; j < 14; j++) {
-      if (i + j < 76800) chunk.pixels[j] = raw_pixels[i + j];
+    uint16_t* raw_pixels = (uint16_t*)fb->buf;
+    IndexedChunk chunk;
+
+    Serial.println("Sending frame...");
+    for (uint32_t i = 0; i < 76800; i += 14) {
+      chunk.pixel_index = i;
+      for (int j = 0; j < 14; j++) {
+        if (i + j < 76800) chunk.pixels[j] = raw_pixels[i + j];
+      }
+      radio.write(&chunk, sizeof(IndexedChunk));
+      delayMicroseconds(150); // Crucial: Gives receiver time to process
     }
-    radio.write(&chunk, sizeof(IndexedChunk));
-    delayMicroseconds(150); // Crucial: Gives receiver time to process
-  }
 
-  esp_camera_fb_return(fb);
-  Serial.println("Frame sent. Waiting 100ms...");
-  delay(100); 
+      esp_camera_fb_return(fb);
+      Serial.println("Frame sent. Waiting 100ms...");
+      delay(100); 
+  }
+  delay(500);
 }
